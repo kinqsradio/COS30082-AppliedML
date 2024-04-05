1947185d1a5afd1646580904eb6a11899b07e161
Tran Duc Anh Dang <anhdang.contact@gmail.com>
2024-04-05 14:30:31 +1100

    update test

---
+++ assignment01/test/test_train.py
@@ -0,0 +1,19 @@
+from torch.utils.data import DataLoader
+from ..dataset import DatasetPreprocessing, create_label_map
+from ..model import BirdClassifier
+from ..train import train_model
+
+if __name__ == '__main__':
+
+    label_map = create_label_map(file_path='data/train.txt')
+    train_dataset = DatasetPreprocessing(dataset_path='data/train', annotation_file='data/train.txt', debug=False, label_map=label_map)
+    test_dataset = DatasetPreprocessing(dataset_path='data/test', annotation_file='data/test.txt', debug=False, label_map=label_map)
+
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
+    val_loader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=4)
+
+    # Initialize the model
+    model = BirdClassifier(num_classes=200, fine_tune_start=5)
+
+    # Train the model
+    trained_model = train_model(model, train_loader, val_loader, num_epochs=20)
--- assignment01/train.py
+++ assignment01/train.py
@@ -1,14 +1,7 @@
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
 import torch.optim as optim
-from torch.optim.lr_scheduler import StepLR
-
 import numpy as np
-import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader
-import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 class EarlyStopping:
@@ -116,22 +109,3 @@ def train_model(model, train_loader, val_loader, num_epochs=25):
     # Load the last checkpoint with the best model
     model.load_state_dict(torch.load('saved/checkpoint.pt'))
     return model
-
-# Debug
-from dataset import DatasetPreprocessing, create_label_map
-from model import BirdClassifier
-
-if __name__ == '__main__':
-
-    label_map = create_label_map(file_path='data/train.txt')
-    train_dataset = DatasetPreprocessing(dataset_path='data/train', annotation_file='data/train.txt', debug=False, label_map=label_map)
-    test_dataset = DatasetPreprocessing(dataset_path='data/test', annotation_file='data/test.txt', debug=False, label_map=label_map)
-
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
-    val_loader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=4)
-
-    # Initialize the model
-    model = BirdClassifier(num_classes=200, fine_tune_start=5)
-
-    # Train the model
-    trained_model = train_model(model, train_loader, val_loader, num_epochs=20)
